ce157af1f83b9a52c5ca459d698932213802aaed
Sealab <sealabjaster@gmail.com>
2013-08-10 03:18:06 +0100

    Filled in some of the AssemblyInfo

--- JDLL/JDLL/Properties/AssemblyInfo.cs
+++ JDLL/JDLL/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JDLL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Libary used by Jaster coders")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JASTER")]
 [assembly: AssemblyProduct("JDLL")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Jaster 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
