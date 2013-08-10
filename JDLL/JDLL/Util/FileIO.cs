9b6f8c7f6cac66a34b3864c50df121d138be8668
Sealab <sealabjaster@gmail.com>
2013-08-10 15:05:42 +0100

    Fixed up the Encryption Array and can now read from a certain line.

--- JDLL/JDLL/Util/FileIO.cs
+++ JDLL/JDLL/Util/FileIO.cs
@@ -11,7 +11,7 @@ namespace JDLL.Util
     {
         #region Encryption
         internal static char[] Letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.', '"', '!', ' ', '(', ')', ';', '=' };
-        internal static String[] Encryption = new String[] { "M7S0P", "O2P4X", "F8k0F", "P3A7S", "Z7C9N", "N4K1D", "X2G8O", "P5D1B", "Q7A2G", "N9H4W", "J4L8I", "M2P1U", "F4W0R", "L4Z0R", "V8T4M", "L0L0L", "Q7D6F", "B0B0X", "E6G6Y", "L0V3Y", "N4T7Q", "X0N7L", "L6Z3B", "G4Y3R", "H4X0R", "K9P8F", "P2P2P", "M0M0M", "D4D3Y", "H8U6V", "T0O0I", "M4N8P", "S0P9Y", "N5A2D", "U5S8N", "S5G2G", "M0P8L", "M1P2H", "K1Z3L", "F4P3Y", "Z0X0R", "A5P9N", "S7G7H", "Z4P3R", "M2A8K", "K4P4P", "N1P3X", "A0X3Y", "C8S0S", "C0C7Y", "M3M3M", "I0N7X", "I9Q3H", "U0I8U", "A8I2F", "D5I9K", "S7D2J", "I0P2L", "C7O2F", "C9I1G", "S5I2G", "U0M4D", "U4P2K", "U1X3C", "U9O0X", "A0W3E", "N0T5M", "M3H2S", "E3D1E", "P0L0R" };
+        internal static String[] Encryption = new String[] { "M7S0P", "O2P4X", "F8K0F", "P3A7S", "Z7C9N", "N4K1D", "X2G8O", "P5D1B", "Q7A2G", "N9H4W", "J4L8I", "M2P1U", "F4W0R", "L4Z0R", "V8T4M", "L0L0L", "Q7D6F", "B0B0X", "E6G6Y", "L0V3Y", "N4T7Q", "X0N7L", "L6Z3B", "G4Y3R", "H4X0R", "K9P8F", "P2P2P", "M0M0M", "D4D3Y", "H8U6V", "T0O0I", "M4N8P", "S0P9Y", "N5A2D", "U5S8N", "S5G2G", "M0P8L", "M1P2H", "K1Z3L", "F4P3Y", "Z0X0R", "A5P9N", "S7G7H", "Z4P3R", "M2A8K", "K4P4P", "N1P3X", "A0X3Y", "C8S0S", "C0C7Y", "M3M3M", "I0N7X", "I9Q3H", "U0I8U", "A8I2F", "D5I9K", "S7D2J", "I0P2L", "C7O2F", "C9I1G", "S5I2G", "U0M4D", "U4P2K", "U1X3C", "U9O0X", "A0W3E", "N0T5M", "M3H2S", "E3D1E", "P0L0R" };
         
         public static void EncryptFile(String Path, String Output, bool DeleteOrigin)
         {
@@ -98,6 +98,13 @@ namespace JDLL.Util
             File.Delete(FileName);
 
             return Array;
+        }
+
+        public static String ReadLine(String Path, int Line)
+        {
+            String[] S = ReadFile(Path);
+
+            return S[Line - 1];
         }
 
         public static void WriteToFile(String Path, String Write)
