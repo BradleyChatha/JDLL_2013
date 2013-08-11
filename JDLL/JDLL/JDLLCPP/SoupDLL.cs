d65693c276175874116d64dab57e84670e650ca4
Sealab <sealabjaster@gmail.com>
2013-08-11 23:09:00 +0100

    Generic Commit

---
+++ JDLL/JDLL/JDLLCPP/SoupDLL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace JDLL.JDLLCPP
+{
+    public class SoupDLL
+    {
+        [DllImport("SoupDLL.dll")]
+        public static extern int Add(int x, int y);
+
+        [DllImport("SoupDLL.dll")]
+        public static extern int Subtract(int x, int y);
+
+        [DllImport("SoupDLL.dll")]
+        public static extern int Multiply(int x, int y);
+
+        [DllImport("SoupDLL.dll")]
+        public static extern int Divide(int x, int y);
+
+        [DllImport("SoupDLL.dll")]
+        public static extern void ShowMessagebox(String Message, String Title);
+    }
+}
---
+++ JDLL/Testerr/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+using JDLL.Data;
+using JDLL.Util;
+using JDLL.JDLLCPP;
+
+namespace Testerr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 6;
+
+            Console.WriteLine(SoupDLL.Add(x, y));
+            Console.WriteLine(SoupDLL.Subtract(x, y));
+            Console.WriteLine(SoupDLL.Multiply(x, y));
+            Console.WriteLine(SoupDLL.Divide(y, x));
+
+            SoupDLL.ShowMessagebox("Message", "Box");
+
+            Console.Read();
+        }
+    }
+}
