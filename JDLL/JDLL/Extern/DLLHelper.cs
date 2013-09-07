f8e1f7ab1a457ba3028ad1ce817f40a38fe22b94
Sealab <sealabjaster@gmail.com>
2013-09-07 19:08:59 +0100

    Some Helper classes

---
+++ JDLL/JDLL/Extern/DLLHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace JDLL.Extern
+{
+    public class DLLHelper
+    {
+        Dictionary<String, String> URLS = new Dictionary<String, String>();
+
+        Dictionary<String, bool> Usuable = new Dictionary<String, bool>();
+
+        public DLLHelper()
+        {
+            Usuable["SoupDLL"] = false;
+        }
+
+        public void DownloadDLL(String Name)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFile(URLS[Name], Name + ".dll");
+            }
+        }
+    }
+}
---
+++ JDLL/JDLL/JDLLHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace JDLL
+{
+    public class JDLLHelper
+    {
+        public const String Version = "1";
+
+        public bool CheckForUpdate()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFile("https://dl.dropbox.com/s/4nm5dmtuukiwc60/Version.txt", "Version.txt");
+            }
+
+            if (File.ReadAllLines("Version.txt")[0].Equals(Version))
+            {
+                File.Delete("Version.txt");
+                return false;
+            }
+            else
+            {
+                File.Delete("Version.txt");
+                return true;
+            }
+        }
+    }
+}
