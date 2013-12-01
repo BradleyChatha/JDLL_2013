49be87b724b6c7921e2b6220871709326abd6bc2
Sealab <sealabjaster@gmail.com>
2013-12-01 00:18:48 +0000

    Start of a new Content pipeline extention

---
+++ JDLL/JDLL/Data/Content/IContentProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JDLL.Data.Content
+{
+    public interface IContentProcessor<T>
+    {
+        public T Import(BinaryReader br);
+        public void Export(BinaryWriter bw);
+    }
+}
