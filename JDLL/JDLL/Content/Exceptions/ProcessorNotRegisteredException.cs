c88b3419f3918938254a31b89c80d33e786299a4
Sealab <sealabjaster@gmail.com>
2013-12-03 07:31:43 +0000

    Initial Commit

---
+++ JDLL/JDLL/Content/Exceptions/ProcessorNotRegisteredException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDLL.Content
+{
+    public class ProcessorNotRegisteredException : Exception
+    {
+        public ProcessorNotRegisteredException(String message) : base(message)
+        {
+
+        }
+    }
+}
