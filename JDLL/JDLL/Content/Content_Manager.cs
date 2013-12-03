7ac565ec938d63c268e52d654417a1cc0a140475
Sealab <sealabjaster@gmail.com>
2013-12-03 03:14:20 +0000

    Added 2 fields, the constructor and a method to register processors

--- JDLL/JDLL/Content/Content_Manager.cs
+++ JDLL/JDLL/Content/Content_Manager.cs
@@ -8,5 +8,18 @@ namespace JDLL.Content
 {
     public class Content_Manager
     {
+        Dictionary<String, IContentProcessor> Processors = new Dictionary<String, IContentProcessor>();
+
+        public String Filename { get; private set; }
+
+        public Content_Manager(String filename)
+        {
+            this.Filename = filename;
+        }
+
+        public void RegisterProcessor(IContentProcessor processor)
+        {
+            this.Processors.Add(processor.TypeName(), processor);
+        }
     }
 }
