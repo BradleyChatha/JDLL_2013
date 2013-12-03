ecbb97123624af151ce6d3349e6e9e490c2db1df
Sealab <sealabjaster@gmail.com>
2013-12-03 03:06:00 +0000

    Initial commit

---
+++ JDLL/JDLL/Content/Processors/IContentProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDLL.Content
+{
+    public abstract class IContentProcessor
+    {
+        public abstract object Import(BinaryReader br);
+        public abstract void Export(BinaryWriter bw, object data);
+    }
+}
