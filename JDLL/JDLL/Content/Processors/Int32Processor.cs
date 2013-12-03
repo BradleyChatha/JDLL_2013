1f4844b80757733482c9c05efe9bc4da3d500171
Sealab <sealabjaster@gmail.com>
2013-12-03 20:05:27 +0000

    New processor for Int32

---
+++ JDLL/JDLL/Content/Processors/Int32Processor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDLL.Content
+{
+    public class Int32Processor : IContentProcessor
+    {
+        public override string TypeName()
+        {
+            return "int32";
+        }
+
+        public override void Export(System.IO.BinaryWriter bw, object data)
+        {
+            bw.Write((int)data);
+        }
+
+        public override object Import(System.IO.BinaryReader br)
+        {
+            return br.ReadInt32();
+        }
+    }
+}
