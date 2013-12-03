41281c83cf3a2a55c496fd8950773a59cdf81358
Sealab <sealabjaster@gmail.com>
2013-12-03 21:07:51 +0000

    Initial commit

---
+++ JDLL/JDLL/Content/Processors/BoolProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDLL.Content
+{
+    public class BoolProcessor : IContentProcessor
+    {
+        public override string TypeName()
+        {
+            return "bool";
+        }
+
+        public override void Export(System.IO.BinaryWriter bw, object data)
+        {
+            bw.Write((bool)data);
+        }
+    }
+}
