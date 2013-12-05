d073b29ddc0598224a17daaea1f63432f5ae5330
Sealab <sealabjaster@gmail.com>
2013-12-05 23:26:30 +0000

    Added new exception to replace the normal one in Content_Manager.Read()

---
+++ JDLL/JDLL/Content/Exceptions/EntryNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDLL.Content
+{
+    public class EntryNotFoundException : Exception
+    {
+        public EntryNotFoundException(String entry) : base("Entry with the name '" + entry + "' does not exist")
+        {
+        }
+    }
+}
