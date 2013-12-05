ec3f0f1225f5f304dc5a7c104d4bce1203af58d5
Sealab <sealabjaster@gmail.com>
2013-12-05 22:22:44 +0000

    Initial Commit

---
+++ JDLL/JDLL/Content/Processors/ByteArrayProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDLL.Content
+{
+    public class ByteArrayProcessor : IContentProcessor
+    {
+        public override string TypeName()
+        {
+            return "byteArray";
+        }
+
+        public override void Export(System.IO.BinaryWriter bw, object data)
+        {
+            byte[] Data = (byte[])data;
+
+            bw.Write(Data.Length);
+            bw.Write(Data);
+        }
+
+        public override object Import(System.IO.BinaryReader br)
+        {
+            Byte[] Data = br.ReadBytes(br.ReadInt32());
+
+            return Data;
+        }
+    }
+}
